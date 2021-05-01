7070b9f08a43ebb91e28f5b44d6375523683aed8
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-05-01 12:13:13 +1000

    indexedDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,7 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/styles.css",
   "/index.js",
+  "/indexedDB.js",
   "/icons/icon-72x72.png",
   "/icons/icon-96x96.png",
   "/icons/icon-128x128.png",
